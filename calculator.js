225489dce56e5a15eacd999e00e0bb25fa78bf73
isnotafunction <ivana.cosgrove@gmail.com>
2018-01-05 17:43:15 +0000

    working buttons

--- calculator.js
+++ calculator.js
@@ -1,69 +1,44 @@
-var one = document.querySelector(".one");
-var two = document.querySelector(".two");
-// var three = document.querySelector(".three");
-// var four = document.querySelector(".four");
-// var five = document.querySelector(".five");
-// var six = document.querySelector(".six");
-// var seven = document.querySelector(".seven");
-// var eight = document.querySelector(".eight");
-// var nine = document.querySelector(".nine");
-// var zero = document.querySelector(".zero");
-// var dot = document.querySelector(".dot");
+var nums = document.querySelectorAll(".numbers");
+var operators=document.querySelectorAll(".operators");
+var display = document.querySelector(".display");
+var clear = document.querySelector(".ac");
+var equals = document.querySelector(".equals");
 
-var display=document.querySelector(".display")
-var ac = document.querySelector(".ac")
-var equals = document.querySelector(".equals")
-
-// var times = document.querySelector(".times");
-// var divide = document.querySelector(".divide");
-// var minus = document.querySelector(".minus");
-var plus = document.querySelector(".plus");
-
-
-var numberArr=[];
-var digit = "";
+var arr = [];
+var digits=''
 var result;
-var operation;
 
-one.addEventListener("click", function(){
-  digit+='1'
-  display.textContent = digit;
-  console.log(digit)
-})
+for(var i=0;i<nums.length;i++){
+  nums[i].addEventListener("click", function(){
+        digits+=this.value;
+          console.log(digits)
+        display.textContent=digits
 
+    })
+}
 
-plus.addEventListener("click", function(){
-numberArr.push(digit);
-operation="+";
-digit=""
-})
+for(var j=0; j<operators.length;j++){
+  operators[j].addEventListener("click", function(){
+    arr.push(digits);
+    digits="";
+    arr.push(this.value);
+    console.log(arr)
+  })
+}
 
-minus.addEventListener("click", function(){
-  numberArr.push(digit);
-  operation="-"
-  digit=""
-})
-
-two.addEventListener("click", function(){
-  digit+='2'
-  display.textContent = digit;
-  console.log(digit)
-})
-
-ac.addEventListener("click", function(){
-  display.textContent="";
-  digit='';
-  numberArr=[];
+clear.addEventListener("click", function(){
+  digits = "";
+  display.textContent = "";
+  arr = [];
 })
 
 equals.addEventListener("click", function(){
-if(digit!=""){
-  numberArr.push(digit)
-}
-console.log(numberArr)
-numberArr=numberArr.map(Number)
-if(operation="+"){
-  result=numberArr.reduce(function(a,b){return a+b},0)
-}
-display.textContent = result;
+  arr.push(digits);
+  console.log(arr)
+  digits="";
+  arr=arr.join("");
+  result = eval(arr);
+  display.textContent = result;
+  arr=[];
+  digits=result;
 })
