f37102b7f6b229454797aca415e1fbc647c866d9
isnotafunction <ivana.cosgrove@gmail.com>
2018-01-27 22:13:38 +0000

    added animation when operator is pressed more than once in a row

--- calculator.js
+++ calculator.js
@@ -19,11 +19,22 @@ for(var i=0;i<nums.length;i++){
 
 for(var j=0; j<operators.length;j++){
   operators[j].addEventListener("click", function(){
-    arr.push(digits);
-    digits="";
-    arr.push(this.value);
+    if(digits!==""){
+     arr.push(digits);
+     digits="";
+    }
+
+  var lastEntry=arr[arr.length-1];
+   if(lastEntry!=="+" && lastEntry!=="-" && lastEntry!=="*" && lastEntry!=="/"){
+     arr.push(this.value);
+   } else {
+     display.classList.add("red")
+     window.setTimeout(function () {
+      display.classList.remove('red');
+}, 1000);
+   }
     console.log(arr)
-  })
+ })
 }
 
 clear.addEventListener("click", function(){
